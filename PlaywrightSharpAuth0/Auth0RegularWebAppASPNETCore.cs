6124c760973af5098e107da8a67edec3ff093acf
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2021-03-31 12:24:36 +0200

    First commit

---
+++ PlaywrightSharpAuth0/Auth0RegularWebAppASPNETCore.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PlaywrightSharp;
+
+namespace PlaywrightSharpAuth0
+{
+    public class Tests
+    {
+        [Test]
+        public async Task Auth0RegularWebAppASPNETCoreLinkWorks()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(headless: true);
+
+            // Go to Auth0 home page
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("https://auth0.com/");
+
+            // Click on the cookie policy acceptance button if it exists
+            if ((await page.QuerySelectorAsync("#truste-consent-button")) != null)
+            {
+                await page.ClickAsync("#truste-consent-button");
+            }
+
+            // Hover on Developers
+            await page.HoverAsync("li[aria-labelledby='nav-item-Developers']");
+
+            // Click on Quickstarts
+            await page.ClickAsync("a[href='https://auth0.com/docs/quickstarts/']");
+
+            // Hover on Regular Web App
+            await page.HoverAsync("div[data-type='webapp']");
+
+            // Click on Launch Quickstart
+            await page.ClickAsync("div[data-type='webapp'] button.btn-success");
+
+            // Search for ASP.NET Core
+            await page.TypeAsync("div.quickstart-search-input input", "ASP.NET Core");
+
+            // Click on ASP.NET Core logo
+            await page.ClickAsync("div[data-name='aspnet-core']");
+
+            // Look for "View on Github" button and get the href property
+            var gitHubLink = await page.EvalOnSelectorAsync<string>("a.btn-transparent.btn-block", "e => e.getAttribute('href')");
+
+            Assert.AreEqual(gitHubLink, "https://github.com/auth0-samples/auth0-aspnetcore-mvc-samples/tree/master/Quickstart/01-Login");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# Playwright Sharp UI test example - Auth0 website
+
+This is a .NET Core Test Project to show how to use [Playwright Sharp](https://github.com/microsoft/playwright-sharp) library for UI testing. We test the [Auth0 website](https://auth0.com/). 
+
+The test verifies that ["Regular Web App" for ASP.NET Core](https://auth0.com/docs/quickstart/webapp/aspnet-core) Quickstart guide is working, and the link to the GitHub repository where the sample code lives is correct.
+
+To run the test, the project uses [NUnit](https://nunit.org/).
+
+# Required technologies
+
+Platform: .NET Core    
+Programming Language: C#    
+Nuget Packages: Playwright Sharp, NUnit
+
+# How to run
+
+## From Command Line
+
+Use NUnit [Console Runner](https://docs.nunit.org/articles/nunit/running-tests/Console-Runner.html).
+
+## From Visual Studio
+
+Use NUnit [Visual Studio Test Adapter](https://docs.nunit.org/articles/vs-test-adapter/Index.html).
+
+## From Visual Studio Code
+
+Use [.NET Core Test Explorer](https://github.com/formulahendry/vscode-dotnet-test-explorer) extension.
